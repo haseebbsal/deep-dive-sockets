87c7528c7b2d2d2f573e97fa810eef38eb03cbaa
shumman <shumman2007@hotmail.com>
2024-04-21 16:03:25 +0500

    implemented sessions

--- socket.js
+++ socket.js
@@ -15,12 +15,48 @@ const io = new Server(app, {
     }
 })
 
+
 io.on('connection', (socket) => {
     console.log('User Connected')
-    console.log(socket.id)
-    socket.on('disconnect', () => {
+    // console.log(socket.id)
+    console.log('connection socketid', socket.handshake.query.sessionid == 'null' ? socket.id : socket.handshake.query.sessionid);
+    console.log('connection pageUrl', socket.handshake.query.pageUrl)
+    socket.on('disconnect', async () => {
+        let sessionid = socket.handshake.query.sessionid == 'null' ? socket.id : socket.handshake.query.sessionid
+        try {
+            const checkingSession = await client.query('SELECT * FROM sessionplayer where sessionid=$1', [sessionid])
+            if (checkingSession.rows[0].completed) {
+                console.log('session is completed so gonna use socket id that is created from server')
+                sessionid = socket.id
+            }
+        }
+        catch(e){
+            console.log(e)
+            console.log('error in checking session on disconnect')
+        }
+        const pageurl = socket.handshake.query.pageUrl
+        console.log('sessionId',sessionid)
         console.log('user disconnected');
-        console.log(socket.id)
+        client.query('SELECT isnavigated,clicktonavigate FROM sessionplayer where sessionid=$1', [sessionid], async (error, results) => {
+            if (error) {
+                console.log('Error in finding Navigaton')
+            }
+            else {
+                if (!results.rows[0].isnavigated) {
+                    console.log('completed session because there was no navigation')
+                    await client.query(`UPDATE sessionplayer SET completed=true WHERE sessionid = $1`,[sessionid])
+                }
+                else {
+                    if (results.rows[0].isnavigated == pageurl) {
+                        console.log('click to navigate', results.rows[0].clicktonavigate)
+                        if (!results.rows[0].clicktonavigate) {
+                            console.log('completed session because there was no navigation after we navigated')
+                            await client.query(`UPDATE sessionplayer SET completed=true WHERE sessionid = $1`, [sessionid])
+                        }
+                    }
+                }
+            }
+        })
     });
 
 
@@ -31,7 +67,8 @@ io.on('connection', (socket) => {
             y: msg.y,
             element: JSON.parse(msg.element),
             ip: msg.ip,
-            userId: msg.userId
+            userId: msg.userId,
+            sessionid: msg.sessionid
         };
         let date = new Date()
         mouseData.timeStamp = `${date.toLocaleTimeString()}`
@@ -41,9 +78,12 @@ io.on('connection', (socket) => {
     });
 
 
+    socket.on('navigationOnUrl', async (msg) => {
+        await client.query(`UPDATE sessionplayer SET clicktonavigate=true WHERE sessionid = $1`, [msg])
+        console.log('clicktonavigate is set to true because we navigated to same page in non SPA')
+    })
 
-
-    socket.on('on-click', (msg) => {
+    socket.on('on-click', async (msg) => {
         console.log('type', msg.type)
         const clickData = {
             type: msg.type,
@@ -51,13 +91,26 @@ io.on('connection', (socket) => {
             y: msg.y,
             element: JSON.parse(msg.element),
             ip: msg.ip,
-            userId: msg.userId
+            userId: msg.userId,
+            sessionid:msg.sessionid
         };
         let date = new Date()
         clickData.timeStamp = `${date.toLocaleTimeString()}`
         clickData.Date = `${date.toLocaleDateString()}`
+        if (msg.isNavigate) {
+            const queryUpdate = `UPDATE sessionplayer SET isNavigated=$2  WHERE sessionid = $1`
+            client.query(queryUpdate, [msg.sessionid,msg.isNavigate], (error, results) => {
+                if (error) {
+                    console.error('Error updating sessionplayer data:', error);
+                } else {
+                    console.log(`updated Navigation in session to ${msg.isNavigate}`)
+                }
+            });
+            // await client.query(`UPDATE sessionplayer SET clicktonavigate=true WHERE sessionid = $1`, [msg.sessionid])
+        }
         insertClickOrUpdateData(clickData);
     });
+
 
     socket.on('AddDomain', (msg) => {
         let updateValues = [msg.userId, msg.domain];
@@ -98,25 +151,56 @@ io.on('connection', (socket) => {
             }
             console.log('userid domain verification search', selectResults.rows)
             if (selectResults.rows.length > 0) {
-                client.query('SELECT isverified FROM userdomains WHERE userid = $1 AND domains=$2', [msg.userId, msg.domain], (selectError, selectResults) => {
+                client.query('SELECT isverified FROM userdomains WHERE userid = $1 AND domains=$2', [msg.userId, msg.domain], async (selectError, selectResults) => {
                     if (selectError) {
                         console.error('Error querying the database:', selectError);
                         return;
                     }
                     console.log('userid domain verification search', selectResults.rows)
-                    socket.emit('serverDomainVerification', selectResults.rows[0].isverified)
-                    
-                 
-                    
+                    // socket.emit('serverDomainVerification', selectResults.rows[0].isverified)
+                    if (selectResults.rows[0].isverified) {
+                        if (!msg.sessionid) {
+                            const sessionCreate = await client.query('INSERT INTO sessionplayer(userid, domain,sessionid) VALUES($1, $2,$3)', [msg.userId, msg.domain, socket.id])
+                            console.log('Session Created sending socket id')
+                            socket.emit('serverDomainVerification', {isverified: selectResults.rows[0].isverified,sessionid:socket.id })
+                        }
+                        else {
+                            client.query('SELECT * FROM sessionplayer where sessionid=$1', [msg.sessionid], async (error, results) => {
+                                if (error) {
+                                    console.log('Error in finding sessionid')
+                                }
+                                else {
+                                    if (results.rows.length > 0) {
+                                        if (results.rows[0].completed) {
+                                            console.log('session is completed , sending new socket id')
+                                            await client.query('INSERT INTO sessionplayer(userid, domain,sessionid) VALUES($1, $2,$3)', [msg.userId, msg.domain, socket.id])
+                                            socket.emit('serverDomainVerification', { isverified: selectResults.rows[0].isverified,sessionid:socket.id })
+                                        }
+                                        else {
+                                            await client.query(`UPDATE sessionplayer SET clicktonavigate=false WHERE sessionid = $1`, [msg.sessionid])
+                                            console.log('session is not completed , so using old socket id')
+                                            socket.emit('serverDomainVerification', { isverified: selectResults.rows[0].isverified })
+                                        }
+                                    }
+                                    else {
+                                        console.log('session id doesnt exist in the table')
+                                        socket.emit('serverDomainVerification', { isverified: selectResults.rows[0].isverified, sessionid: socket.id })
+                                    }
+                                }
+                            })
+                        }
+                    }
+                    else {
+                        socket.emit('serverDomainVerification', { isverified: selectResults.rows[0].isverified })
+                    }
                 });
             }
             else {
-                socket.emit('serverDomainVerification', false)
+                socket.emit('serverDomainVerification', { isverified: false })
             }
         });
         console.log('clientUserVerification', msg)
     });
-
 
 
     socket.on('reportIP', (msg) => {
--- utils/insert-click-update.js
+++ utils/insert-click-update.js
@@ -1,7 +1,7 @@
 const client = require('../db-connection')
 function insertClickOrUpdateData(clickData) {
-    const selectQuery = `SELECT * FROM client_interaction_data WHERE user_ipaddress = $1 AND x_cordinate = $2 AND y_cordinate = $3 AND element = $4 AND action_type = $5 `;
-    const selectValues = [clickData.ip, clickData.x, clickData.y, clickData.element, clickData.type];
+    const selectQuery = `SELECT * FROM client_interaction_data WHERE user_ipaddress = $1 AND x_cordinate = $2 AND y_cordinate = $3 AND element = $4 AND action_type = $5 AND sessionid=$6`;
+    const selectValues = [clickData.ip, clickData.x, clickData.y, clickData.element, clickData.type,clickData.sessionid];
 
     client.query(selectQuery, selectValues, (selectError, selectResults) => {
         if (selectError) {
@@ -13,14 +13,14 @@ function insertClickOrUpdateData(clickData) {
         // console.log('Records retrieved:', selectResults.rows);
         let findingIfExists = selectResults.rows.length > 0 ? true : false
         // console.log(findingIfExists)
-        const query = 'INSERT INTO client_interaction_data(x_cordinate, y_cordinate,action_type, element, user_ipaddress, date, time,count,userid) VALUES($1, $2, $3, $4, $5 ,$6 ,$7,$8,$9)';
-        const values = [clickData.x, clickData.y, clickData.type, clickData.element, clickData.ip, clickData.Date, clickData.timeStamp, 1, clickData.userId];
+        const query = 'INSERT INTO client_interaction_data(x_cordinate, y_cordinate,action_type, element, user_ipaddress, date, time,count,userid,sessionid) VALUES($1, $2, $3, $4, $5 ,$6 ,$7,$8,$9,$10)';
+        const values = [clickData.x, clickData.y, clickData.type, clickData.element, clickData.ip, clickData.Date, clickData.timeStamp, 1, clickData.userId, clickData.sessionid];
         if (!findingIfExists) {
             client.query(query, values, (error, results) => {
                 if (error) {
                     // console.error('Error inserting click event data:', error);
                 } else {
-                    console.log(`${clickData.type} event data inserted ${values}`);
+                    // console.log(`${clickData.type} event data inserted ${values}`);
                 }
                 // client.end()
             });
@@ -29,14 +29,14 @@ function insertClickOrUpdateData(clickData) {
 
             let count = Number(selectResults.rows[0].count)
             // console.log(count)
-            let updateValues = [clickData.ip, clickData.x, clickData.y, clickData.element, count + 1, clickData.userId];
-            const queryUpdate = `UPDATE client_interaction_data SET count=$5  WHERE user_ipaddress = $1 AND x_cordinate = $2 AND y_cordinate = $3 AND element = $4 AND userid = $6`
+            let updateValues = [clickData.ip, clickData.x, clickData.y, clickData.element, count + 1, clickData.userId, clickData.sessionid];
+            const queryUpdate = `UPDATE client_interaction_data SET count=$5  WHERE user_ipaddress = $1 AND x_cordinate = $2 AND y_cordinate = $3 AND element = $4 AND userid = $6 AND sessionid=$7`
             client.query(queryUpdate, updateValues, (error, results) => {
                 if (error) {
                     console.error('Error inserting click event data:', error);
                 } else {
 
-                    console.log(`${clickData.type} event data updated ${values}`);
+                    // console.log(`${clickData.type} event data updated ${values}`);
                 }
                 // client.end()
             });
